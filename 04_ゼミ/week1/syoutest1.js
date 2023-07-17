1f1471eba49a692e00b1178aeb534da6a4ea2f79
Koshi41 <kossy1009@icloud.com>
2023-07-17 20:04:50 +0900

    コミット

---
+++ 04_ゼミ/week1/syoutest1.js
@@ -0,0 +1,16 @@
+const character = {
+    name:"namihei",
+    age: 68,
+    mother.name: "12345",
+    hobbies:"bonsai","bakamon","aho"
+}
+
+const multiply = function(a,b){
+    return a * b
+}
+
+for (let n = 1; n <= 10; n++) {
+    if (n % 2 === 0) {
+      console.log(n)
+    } 
+  }
